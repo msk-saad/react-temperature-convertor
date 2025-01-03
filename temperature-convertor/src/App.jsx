f093f1fc0fcd1a32e376e2dfdc27dff9fd778be4
msk-saad <developersaad12@gmail.com>
2025-01-03 13:36:13 +0530

    Initializing project

---
+++ temperature-convertor/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/app.css'
+
+const App = () => {
+
+  const title = "Temperature Convertor 🔥"
+  return (
+    <>
+      <h1 className='title'>{title}</h1>
+    </>
+  )
+}
+
+export default App
